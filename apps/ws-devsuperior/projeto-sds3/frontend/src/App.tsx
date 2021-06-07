f3db3fb4153478cbcc72555622a4af1b00dad480
Charles Borges <solucao.erp@gmail.com>
2021-06-06 21:54:03 -0300

    Primeira Etapa do Projeto concluida - Populado de forma estática

--- apps/ws-devsuperior/projeto-sds3/frontend/src/App.tsx
+++ apps/ws-devsuperior/projeto-sds3/frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Aplicativo Web React [App.tsx]</h1>
-    </div>
+    <>
+
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Ponto de Chamada dos Componentes [App.tsx]</h1>
+        <DataTable />
+      </div>
+      <Footer />
+
+    </>
   );
 }
 
