3b6a40b0ad52f7e2106f13668ef2e0dfbbdfdd6c
Charles Borges <solucao.erp@gmail.com>
2022-08-16 18:39:42 -0300

    backend: start project

---
+++ crud-sample-001/backend/src/main/java/com/ibrplanner/IbrPlannerNameApplication.java
@@ -0,0 +1,13 @@
+package com.ibrplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbrPlannerNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbrPlannerNameApplication.class, args);
+	}
+
+}
---
+++ crud-sample-001/backend/src/test/java/com/ibrplanner/IbrPlannerNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbrPlannerNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
