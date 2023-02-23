77cfc1b27a3f5a5700866dc471e575c2632742d3
Charles Borges <solucao.erp@gmail.com>
2023-02-23 19:41:43 -0300

    Add: H2 Database, Dependências; Config: CategoriaModel

--- pedidosUML/src/main/java/com/ibrplanner/pedidos/entities/CategoriaModel.java
+++ pedidosUML/src/main/java/com/ibrplanner/pedidos/entities/CategoriaModel.java
@@ -1,10 +1,16 @@
 package com.ibrplanner.pedidos.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "categoria")
 public class CategoriaModel implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
 
