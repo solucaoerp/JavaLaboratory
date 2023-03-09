0cd7aeaa36b715e85606cfec54a76cae270bce0f
Charles Borges <solucao.erp@gmail.com>
2023-03-09 14:38:10 -0300

    Configurações de Perfis e Variáveis de Ambiente

--- pedidosUML/src/main/java/com/ibrplanner/pedidos/PedidosApplication.java
+++ pedidosUML/src/main/java/com/ibrplanner/pedidos/PedidosApplication.java
@@ -2,12 +2,16 @@ package com.ibrplanner.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@PropertySource("classpath:env.properties")
 public class PedidosApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PedidosApplication.class, args);
+        SpringApplication app = new SpringApplication(PedidosApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        app.setAdditionalProfiles(env.getProperty("spring.profiles.active"));
     }
-
 }
