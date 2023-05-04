bc3c231ab87a1cc4cf1534b4a439650923ad8990
Charles Borges <solucao.erp@gmail.com>
2023-05-04 19:45:12 -0300

    SQL para testes (import.sql)

---
+++ prototypes/intermediate/persistence-orm-h2/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_department (name) VALUES ('TECH');
+INSERT INTO tb_department (name) VALUES ('PET');
+
+INSERT INTO tb_product (name, price, department_id) VALUES ('MACKBOOK PRO', 4000.0, 1);
+INSERT INTO tb_product (name, price, department_id) VALUES ('PC GAMER', 5000.0, 1);
+INSERT INTO tb_product (name, price, department_id) VALUES ('PET HOUSE', 300.0, 2);
