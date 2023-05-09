9999c84487c547fab3a30417077d8c9a5125e589
Charles Borges <solucao.erp@gmail.com>
2023-05-08 22:23:26 -0300

    GameController, GameService, GameDTO, DTO

---
+++ prototypes/intermediate/dslist/src/main/java/com/ibrplanner/dslist/controllers/GameController.java
@@ -0,0 +1,26 @@
+package com.ibrplanner.dslist.controllers;
+
+import com.ibrplanner.dslist.dtos.GameDTO;
+import com.ibrplanner.dslist.entities.Game;
+import com.ibrplanner.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameDTO> findAll() {
+        List<GameDTO> result = gameService.findAll();
+        return result;
+    }
+
+}
--- prototypes/intermediate/dslist/src/main/java/com/ibrplanner/dslist/services/GameService.java
+++ prototypes/intermediate/dslist/src/main/java/com/ibrplanner/dslist/services/GameService.java
@@ -1,5 +1,6 @@
 package com.ibrplanner.dslist.services;
 
+import com.ibrplanner.dslist.dtos.GameDTO;
 import com.ibrplanner.dslist.entities.Game;
 import com.ibrplanner.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +14,9 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
-    public List<Game> findAll() {
+    public List<GameDTO> findAll() {
         List<Game> result = gameRepository.findAll();
-
-        return result;
+        List<GameDTO> dto = result.stream().map(x -> new GameDTO(x)).toList();
+        return dto;
     }
 }
