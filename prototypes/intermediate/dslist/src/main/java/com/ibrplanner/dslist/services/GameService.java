f75251682c3cdd2141ec554281285a5bb8376557
Charles Borges <solucao.erp@gmail.com>
2023-05-08 22:13:56 -0300

    GameService

---
+++ prototypes/intermediate/dslist/src/main/java/com/ibrplanner/dslist/services/GameService.java
@@ -0,0 +1,21 @@
+package com.ibrplanner.dslist.services;
+
+import com.ibrplanner.dslist.entities.Game;
+import com.ibrplanner.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<Game> findAll() {
+        List<Game> result = gameRepository.findAll();
+
+        return result;
+    }
+}
