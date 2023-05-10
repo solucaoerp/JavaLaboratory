a0252d4eeaa8879f050d2b4cd94ee3438e48f9b4
Charles Borges <solucao.erp@gmail.com>
2023-05-09 21:08:45 -0300

    Model Domain, Belonging, BelongingPK, GameList mapping

--- prototypes/intermediate/dslist/src/main/java/com/ibrplanner/dslist/entities/Belonging.java
+++ prototypes/intermediate/dslist/src/main/java/com/ibrplanner/dslist/entities/Belonging.java
@@ -1,9 +1,57 @@
 package com.ibrplanner.dslist.entities;
 
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_belonging")
 public class Belonging {
 
-    private Game game;
-    private GameList gameList;
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
 
     private Integer position;
+
+    public Belonging() {
+    }
+
+    public Belonging(Game game, GameList list, Integer position) {
+        id.setGame(game);
+        id.setGameList(list);
+        this.position = position;
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public void setId(BelongingPK id) {
+        this.id = id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Belonging belonging = (Belonging) o;
+
+        return Objects.equals(id, belonging.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
