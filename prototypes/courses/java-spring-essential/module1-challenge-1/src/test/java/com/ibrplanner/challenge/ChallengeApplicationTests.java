a7c71bc8ca1bc793dc22923201a3367d20b7cea3
Charles Borges <solucao.erp@gmail.com>
2023-05-17 21:34:48 -0300

    Desafio: Componentes e injecao de dependencia criado

---
+++ prototypes/courses/java-spring-essential/module1-challenge-1/src/test/java/com/ibrplanner/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrplanner.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
