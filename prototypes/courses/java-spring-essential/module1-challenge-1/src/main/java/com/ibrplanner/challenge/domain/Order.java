4096a0cdbe37b245ebba883855ffb1b28fd1886d
Charles Borges <solucao.erp@gmail.com>
2023-05-18 07:30:03 -0300

    Classe de dominio 'Order' criada

---
+++ prototypes/courses/java-spring-essential/module1-challenge-1/src/main/java/com/ibrplanner/challenge/domain/Order.java
@@ -0,0 +1,40 @@
+package com.ibrplanner.challenge.domain;
+
+public class Order {
+    private int code;
+    private double basic;
+    private double discount;
+
+    public Order() {
+    }
+
+    public Order(int code, double basic, double discount) {
+        this.code = code;
+        this.basic = basic;
+        this.discount = discount;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public double getBasic() {
+        return basic;
+    }
+
+    public void setBasic(double basic) {
+        this.basic = basic;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+}
