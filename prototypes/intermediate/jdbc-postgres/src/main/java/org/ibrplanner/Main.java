4fb83f2b4ca7427644c83946d86533356cdb77df
Charles Borges <solucao.erp@gmail.com>
2023-05-21 16:12:59 -0300

    Composicao e Mapeamento (Order-Product), JDBC Vanilla

--- prototypes/intermediate/jdbc-postgres/src/main/java/org/ibrplanner/Main.java
+++ prototypes/intermediate/jdbc-postgres/src/main/java/org/ibrplanner/Main.java
@@ -9,38 +9,67 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
 
         Connection conn = DB.getConnection();
-
         Statement st = conn.createStatement();
 
-        ResultSet rs = st.executeQuery("select * from tb_order");
+        ResultSet rs = st.executeQuery("SELECT * FROM tb_order " +
+                "INNER JOIN tb_order_product ON tb_order.id = tb_order_product.order_id " +
+                "INNER JOIN tb_product ON tb_product.id = tb_order_product.product_id");
+
+        Map<Long, Order> orders = new HashMap<>();
+        Map<Long, Product> products = new HashMap<>();
 
         while (rs.next()) {
-            Order order = instantiateOrder(rs);
-            System.out.println(order.toString());
+
+            Long orderId = rs.getLong("order_id");
+            if (orders.get(orderId) == null) {
+                Order order = instantiateOrder(rs);
+                orders.put(orderId, order);
+            }
+
+            Long productId = rs.getLong("product_id");
+            if (products.get(productId) == null) {
+                Product product = instantiateProduct(rs);
+                products.put(productId, product);
+            }
+
+            // Associando Order com Product
+            orders.get(orderId).getProducts().add(products.get(productId));
+        }
+        // Order
+        for (Long orderId : orders.keySet()) {
+            System.out.println(orders.get(orderId));
+
+            // Product
+            for (Product product : orders.get(orderId).getProducts()) {
+                System.out.println(product);
+            }
+            System.out.println();
         }
     }
 
     private static Product instantiateProduct(ResultSet rs) throws SQLException {
-        Product p = new Product();
+        Product product = new Product();
 
-        p.setId(rs.getLong("id"));
-        p.setName(rs.getString("name"));
-        p.setPrice(rs.getDouble("price"));
-        p.setDescription(rs.getString("description"));
-        p.setImageUri(rs.getString("image_uri"));
+        product.setId(rs.getLong("product_id"));
+        product.setName(rs.getString("name"));
+        product.setPrice(rs.getDouble("price"));
+        product.setDescription(rs.getString("description"));
+        product.setImageUri(rs.getString("image_uri"));
 
-        return p;
+        return product;
     }
 
     private static Order instantiateOrder(ResultSet rs) throws SQLException {
         Order order = new Order();
 
-        order.setId(rs.getLong("id"));
+        order.setId(rs.getLong("order_id"));
         order.setLatitude(rs.getDouble("latitude"));
         order.setLongitude(rs.getDouble("longitude"));
         order.setMoment(rs.getTimestamp("moment").toInstant());
