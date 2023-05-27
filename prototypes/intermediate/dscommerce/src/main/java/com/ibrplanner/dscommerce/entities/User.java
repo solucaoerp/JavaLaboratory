b25666f706cde563756b90d86bb79ec9aecfce8c
Charles Borges <solucao.erp@gmail.com>
2023-05-27 20:57:19 -0300

    Perfil 'test' (H2 Database), mapeamento User

--- prototypes/intermediate/dscommerce/src/main/java/com/ibrplanner/dscommerce/entities/User.java
+++ prototypes/intermediate/dscommerce/src/main/java/com/ibrplanner/dscommerce/entities/User.java
@@ -1,9 +1,14 @@
 package com.ibrplanner.dscommerce.entities;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "tb_user")
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
