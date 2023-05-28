d2b924e3989050e20ee160371627767a5870a029
Charles Borges <solucao.erp@gmail.com>
2023-05-28 16:42:52 -0300

    Definindo a entidade (dominio) Category

---
+++ prototypes/intermediate/dscommerce/src/main/java/com/ibrplanner/dscommerce/entities/Category.java
@@ -0,0 +1,36 @@
+package com.ibrplanner.dscommerce.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
